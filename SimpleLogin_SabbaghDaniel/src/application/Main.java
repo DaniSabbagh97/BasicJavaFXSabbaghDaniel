cb35ca685fc10dd38dacc799066eb4b33f1061f6
minif <minif@192.168.1.128>
2018-12-06 17:27:46 +0100

    asdd

--- SimpleLogin_SabbaghDaniel/src/application/Main.java
+++ SimpleLogin_SabbaghDaniel/src/application/Main.java
@@ -1,26 +1,44 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.stage=primaryStage;
+        mainWindow();
+    }
+
+    public void mainWindow(){
+        try {
+
+            FXMLLoader loader=new FXMLLoader(Main.class.getResource("sample.fxml"));
+            AnchorPane pane=loader.load();
+            Scene scene=new Scene(pane);
+        scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+        stage.setTitle("signin");
+        stage.setScene(scene);
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
